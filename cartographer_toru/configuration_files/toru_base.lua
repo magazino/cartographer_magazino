918eac4963c8a57edc2f5167f5c501d2b6206734
Michael Grupp <grupp@magazino.eu>
2018-02-27 14:08:47 +0100

    Works with cartographer_ros 0.3.0.2

--- cartographer_toru/configuration_files/toru_base.lua
+++ cartographer_toru/configuration_files/toru_base.lua
@@ -20,9 +20,8 @@ options = {
   published_frame = "odom",
   odom_frame = "odom",
   provide_odom_frame = false,
-  publish_frame_projected_to_2d = true,
+  project_pose_to_2d_plane = true,
   use_odometry = true,
-  use_nav_sat = false,
   num_laser_scans = 2,
   num_multi_echo_laser_scans = 0,
   num_subdivisions_per_laser_scan = 16,
@@ -33,7 +32,6 @@ options = {
   trajectory_publish_period_sec = 0.1,
   rangefinder_sampling_ratio = 1.,
   odometry_sampling_ratio = 1.,
-  fixed_frame_pose_sampling_ratio = 1.,
   imu_sampling_ratio = 1.,
 }
 
